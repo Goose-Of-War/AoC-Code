0a9c2bb3465a77bf1b5dceea561098f333d7157c
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2022-12-12 16:03:16 +0530

    Bug Fixes: Day 12

--- 2022/day12part2.js
+++ 2022/day12part2.js
@@ -10,61 +10,66 @@ require('fs').readFile('./2022/day12input.txt', (err, buf) => {
 	].join(' ');
 	input[input.findIndex(row => row.find(e => e === 'E'))][input[input.findIndex(row => row.find(e => e === 'E'))].findIndex(e => e === 'E')] = 'z';
 	const records = [];
-	for (let i = 0; i < input.length; i++ ) for (let j = 0; j < input[i].length; j++) { 
-		console.log(i, j);
-		if (input[i][j] !== 'a') continue;
+	for (let i = 0; i < input.length; i++ ) {
+		for (let j = 0; j < input[i].length; j++) { 
+			console.log(i, j);
+			if (input[i][j] !== 'a') continue;
 
-		const Iterations = [];
+			const Iterations = [];
 
-		function crawlIteration() {
-			const precedence = 'SabcdefghijklmnopqrstuvwxyzE';
-			const iter = new Set();
-			const prevIter = Iterations.pop();
-			Iterations.push(prevIter);
-			let breakChecker = true;
-			[...prevIter].forEach(pos => iter.add(pos));
-			[...prevIter].forEach(pos => {
-				const coord = pos.split(' ').map(e => ~~e);
-				if (coord[0] - 1 >= 0) {
-					if (precedence.indexOf(input[coord[0] - 1][coord[1]]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
-						if (!iter.has([coord[0] - 1, coord[1]].join(' '))) breakChecker = false;
-						iter.add([coord[0] - 1, coord[1]].join(' '));
+			function crawlIteration() {
+				const precedence = 'SabcdefghijklmnopqrstuvwxyzE';
+				const iter = new Set();
+				const prevIter = Iterations.pop();
+				Iterations.push(prevIter);
+				let breakChecker = true;
+				[...prevIter].forEach(pos => iter.add(pos));
+				[...prevIter].forEach(pos => {
+					const coord = pos.split(' ').map(e => ~~e);
+					if (coord[0] - 1 >= 0) {
+						if (precedence.indexOf(input[coord[0] - 1][coord[1]]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
+							if (!iter.has([coord[0] - 1, coord[1]].join(' '))) breakChecker = false;
+							iter.add([coord[0] - 1, coord[1]].join(' '));
+						}
 					}
-				}
-				if (coord[0] + 1 < input.length) {
-					if (precedence.indexOf(input[coord[0] + 1][coord[1]]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
-						if (!iter.has([coord[0] + 1, coord[1]].join(' '))) breakChecker = false;
-						iter.add([coord[0] + 1, coord[1]].join(' '));
+					if (coord[0] + 1 < input.length) {
+						if (precedence.indexOf(input[coord[0] + 1][coord[1]]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
+							if (!iter.has([coord[0] + 1, coord[1]].join(' '))) breakChecker = false;
+							iter.add([coord[0] + 1, coord[1]].join(' '));
+						}
 					}
-				}
-				if (coord[1] - 1 >= 0) {
-					if (precedence.indexOf(input[coord[0]][coord[1] - 1]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
-						if (!iter.has([coord[0], coord[1] - 1].join(' '))) breakChecker = false;
-						iter.add([coord[0], coord[1] - 1].join(' '));
+					if (coord[1] - 1 >= 0) {
+						if (precedence.indexOf(input[coord[0]][coord[1] - 1]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
+							if (!iter.has([coord[0], coord[1] - 1].join(' '))) breakChecker = false;
+							iter.add([coord[0], coord[1] - 1].join(' '));
+						}
 					}
-				}
-				if (coord[1] + 1 < input[0].length) {
-					if (precedence.indexOf(input[coord[0]][coord[1] + 1]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
-						if (!iter.has([coord[0], coord[1] + 1].join(' '))) breakChecker = false;
-						iter.add([coord[0], coord[1] + 1].join(' '));
+					if (coord[1] + 1 < input[0].length) {
+						if (precedence.indexOf(input[coord[0]][coord[1] + 1]) - precedence.indexOf(input[coord[0]][coord[1]]) <= 1) {
+							if (!iter.has([coord[0], coord[1] + 1].join(' '))) breakChecker = false;
+							iter.add([coord[0], coord[1] + 1].join(' '));
+						}
 					}
-				}
-			});
-			Iterations.push(iter);
-			// console.log(Iterations.length);
-			return breakChecker;
-		};
-		
-		Iterations.push(new Set());
-		// store start position, then it replace with its elevation
-		Iterations[0].add([i, j].join(' '));
-		
-		while (!Iterations[Iterations.length - 1].has(EndPosition)) {
-			if (crawlIteration()) break;
+				});
+				Iterations.push(iter);
+				// console.log(Iterations.length);
+				return breakChecker;
+			};
 			
-		};
-		records.push(Iterations.length - 1);
-		// if (i === 20 && j === 0) console.log(Iterations.length - 1);
+			Iterations.push(new Set());
+			// store start position, then it replace with its elevation
+			Iterations[0].add([i, j].join(' '));
+			let skip ;
+			while (!Iterations[Iterations.length - 1].has(EndPosition)) {
+				skip = crawlIteration();
+				if (skip) break;
+				
+			};
+			if (skip) continue;
+			records.push(Iterations.length - 1);
+			// if (i === 20 && j === 0) console.log(Iterations.length - 1);
+		}
 	}
 	console.log('Result:',records.reduce((a, b) => a > b ? b : a));
+	// In case you're wondering, yes. This code was a load of baloney until last commit. I just reverse engineered the solution from the first part's answer. Now, it's fixed.
 });
